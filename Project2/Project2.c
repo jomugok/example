567d4228be7fd2db5726c110125d9361277f8f33
kosa <kosa@DESKTOP-RQE37P8>
2025-04-23 17:02:44 +0900

    초기 커밋.

---
+++ Project2/Project2.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>         
+
+int main(void)                  
+{
+    int num1, num2;  // 변수 num1, num2 선언
+
+    int num3 = 30, num4 = 40; // 변수 num3, num4의 선언과 초기화
+    
+    printf("num: %d, num2: %d \n", num1, num2); 
+
+    num1 = 10;
+    num2 = 20;
+
+    printf("num1: %d, num2: %d \n", num1, num2);
+    printf("num3: %d, num4: %d \n", num3, num4);
+    
+    return 0;                  
+}                              
