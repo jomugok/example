53129508a5c51d044aaf0c8d195f8f94329c4f5c
sueun <sueun1201@gmail.com>
2025-04-23 17:23:20 +0900

    코드변경테스트용커밋

--- Project2/Project2.c
+++ Project2/Project2.c
@@ -2,17 +2,14 @@
 
 int main(void)                  
 {
-    int num1, num2;  // 변수 num1, num2 선언
-
-    int num3 = 30, num4 = 40; // 변수 num3, num4의 선언과 초기화
+    int num1 = 3;
+    int num2 = 4;
     
-    printf("num: %d, num2: %d \n", num1, num2); 
+    int result = num1 + num2;
 
-    num1 = 10;
-    num2 = 20;
+    printf("덧셈 결과: %d\n", result);
+    printf("%d + %d = %d\n", num1, num2, result);
+    printf("%d와 %d의 합은 %d입니다.\n", num1, num2, result);
 
-    printf("num1: %d, num2: %d \n", num1, num2);
-    printf("num3: %d, num4: %d \n", num3, num4);
-    
     return 0;                  
 }                              
